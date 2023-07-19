5c82b65dd23f7eb1fbe02689ae608b5c0c27b21c
Facundo Chavez <101282065+facukun@users.noreply.github.com>
2023-07-19 03:18:07 -0300

    v1.0.4.3

--- pages/contents/YouTubeParallax.content/YouTubeParallax.content.jsx
+++ pages/contents/YouTubeParallax.content/YouTubeParallax.content.jsx
@@ -5,6 +5,7 @@ import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 
 const YoutubeParallax = () => {
+  const parallaxRef = useRef();
   const [tailLength, setTailLength] = useState(0);
   const topTextRef = useRef();
   const bottomContainerRef = useRef();
@@ -14,7 +15,7 @@ const YoutubeParallax = () => {
       const topTextRect = topTextRef.current.getBoundingClientRect();
       const bottomContainerRect = bottomContainerRef.current.getBoundingClientRect();
 
-      const extraTailLength = window.innerWidth < 820 ? 15 : 25;
+      const extraTailLength = window.innerWidth < 820 ? 15 : 35;
       const newTailLength = bottomContainerRect.top - topTextRect.bottom + extraTailLength;
       console.log(bottomContainerRect.top, topTextRect.bottom);
       setTailLength(newTailLength);
@@ -22,14 +23,31 @@ const YoutubeParallax = () => {
     handleTailLength();
     addEventListener('resize', handleTailLength);
 
+    const parallax = parallaxRef.current;
+
+    const handleParallaxRotation = (event, element) => {
+      const { clientX, clientY } = event;
+      const middleX = window.innerWidth / 2;
+      const middleY = window.innerHeight / 2;
+      const offsetX = (clientX - middleX) / middleX;
+      const offsetY = (clientY - middleY) / middleY;
+      const rotateX = offsetY * -8;
+      const rotateY = offsetX * 8;
+
+      element.style.setProperty('--rotateX', rotateX + 'deg');
+      element.style.setProperty('--rotateY', rotateY + 'deg');
+    };
+    addEventListener('mousemove', (e) => handleParallaxRotation(e, parallax));
+
     return () => {
       removeEventListener('resize', handleTailLength);
+      removeEventListener('mousemove', handleParallaxRotation);
     };
   }, []);
 
   //// COMPONENT
   return (
-    <div className={styles.youtube_parallax}>
+    <div className={styles.youtube_parallax} ref={parallaxRef}>
       <div className={styles.youtube_parallax__top_container}>
         <div className={styles.youtube_parallax__top_container__top_text} ref={topTextRef}>
           <TextBox
