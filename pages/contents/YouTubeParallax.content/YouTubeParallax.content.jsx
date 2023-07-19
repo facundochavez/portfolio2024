fb071aeb2644b78172a0525c4e3e4584fd656f45
Facundo Chavez <101282065+facukun@users.noreply.github.com>
2023-07-19 00:44:54 -0300

    v1.0.4.2

--- pages/contents/YouTubeParallax.content/YouTubeParallax.content.jsx
+++ pages/contents/YouTubeParallax.content/YouTubeParallax.content.jsx
@@ -2,6 +2,7 @@ import TextBox from '@/components/TextBox/TextBox';
 import styles from './YouTubeParallax.content.module.scss';
 import milestones from '@/data/milestones.data.json';
 import { useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
 
 const YoutubeParallax = () => {
   const [tailLength, setTailLength] = useState(0);
@@ -13,7 +14,8 @@ const YoutubeParallax = () => {
       const topTextRect = topTextRef.current.getBoundingClientRect();
       const bottomContainerRect = bottomContainerRef.current.getBoundingClientRect();
 
-      const newTailLength = bottomContainerRect.top - topTextRect.bottom + 15;
+      const extraTailLength = window.innerWidth < 820 ? 15 : 25;
+      const newTailLength = bottomContainerRect.top - topTextRect.bottom + extraTailLength;
       console.log(bottomContainerRect.top, topTextRect.bottom);
       setTailLength(newTailLength);
     };
@@ -28,16 +30,23 @@ const YoutubeParallax = () => {
   //// COMPONENT
   return (
     <div className={styles.youtube_parallax}>
-      <div className={styles.youtube_parallax__top_container} >
+      <div className={styles.youtube_parallax__top_container}>
         <div className={styles.youtube_parallax__top_container__top_text} ref={topTextRef}>
-          <TextBox text={milestones.filter((milestone) => milestone.id === 'youtube')[0].topText} tailLength={tailLength} />
+          <TextBox
+            text={milestones.find((milestone) => milestone.id === 'youtube').topText}
+            tailLength={tailLength}
+          />
         </div>
-        <div className={styles.youtube_parallax__top_container__channel_video}>
-          <div className={styles.youtube_parallax__top_container__channel_video__video} />
+        <div className={styles.youtube_parallax__top_container__mo_channel}>
+          <div className={styles.youtube_parallax__top_container__mo_channel__video}>
+            <Image src={`/videos/image-mo-channel.jpg`} alt={`Marca Optimizada's video.`} fill />
+          </div>
         </div>
       </div>
-      <div className={styles.youtube_parallax__bottom_container} ref={bottomContainerRef}>
-        <div className={styles.youtube_parallax__bottom_container__website_video} />
+      <div className={styles.youtube_parallax__mo_website} ref={bottomContainerRef}>
+        <div className={styles.youtube_parallax__mo_website__image}>
+          <Image src={`/videos/image-mo-website.jpg`} alt={`Marca Optimizada's website.`} fill />
+        </div>
       </div>
     </div>
   );
