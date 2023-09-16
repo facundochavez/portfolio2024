cf99b5d7dd19a138387c24f1311d4ffaaaefa54b
Facundo Chavez <101282065+facukun@users.noreply.github.com>
2023-09-16 17:13:18 -0300

    v1.9.7

--- pages/layouts/Milestone.layout/contents/BackendVideo.content/BackendVideo.content.jsx
+++ pages/layouts/Milestone.layout/contents/BackendVideo.content/BackendVideo.content.jsx
@@ -4,7 +4,7 @@ const BackendVideo = () => {
   //// COMPONENT
   return (
     <div className={styles.backend_video}>
-      <a href='http://www.petpath.app' target='_blank'>
+      <a href='https://petpath.vercel.app/' target='_blank'>
         <div className={styles.backend_video__wrapper}>
           <video muted autoPlay loop src='/videos/video-petpath.mp4' />
         </div>
